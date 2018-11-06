9c9645d795998626184948a87475e5d78e11a5e7
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-06 14:29:28 -0500

    message

--- client/index.js
+++ client/index.js
@@ -8,6 +8,9 @@ import Main from './components'
 ReactDOM.render(
   <div id="main-container">
     <h1 id="title">Welcome to the Mars Mining Project!</h1>
+    <h3>Bots: Red</h3>
+    <h3>Nodes: Blue</h3>
+    <h3>Both: Purple</h3>
     <Main/>
   </div>,
   document.getElementById(`root`)
