351ae5c57411d5b1396630feaccd709578ac9c73
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-06 14:15:05 -0500

    feat: processes/organizes data, displays a row

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,33 +1,63 @@
 import React from 'react'
 import axios from 'axios'
+import shortid from 'shortid'
+
+import Row from './Row'
 
 class Map extends React.Component{
   constructor(){
     super()
     this.state = {
       intervalId : 0,
-      bots : [],
-      nodes : [],
+      bots : {},
+      nodes : {},
     }
   }
+
+  // CONSTANTS START
+
+  ROWS = 20
+
+  COLUMNS = 20
+
+  // CONSTANTS END
 
   // LIFECYCLE METHODS START
 
   componentDidMount = () => {
     const makeCalls = async () => {
-      try{
-        const bots = await axios.get(`https://headlight-tournament-1.herokuapp.com/bots`)
-        const nodes = await axios.get(`https://headlight-tournament-1.herokuapp.com/nodes`)
-        this.setState({
-          bots : bots.data,
-          nodes : nodes.data,
+      const processApiData = apiData => {
+        const newStateField = {}
+        apiData.forEach(item => {
+          const row = item.Location.Y
+          const col = item.Location.X
+          if(newStateField[row]){
+            if(newStateField[row][col]){
+              newStateField[row][col].push(item)
+            }else{
+              newStateField[row][col] = [item]
+            }
+          }else{
+            newStateField[row] = {[col] : [item]}
+          }
         })
+        return newStateField
+      }
+
+      try{
+        const botsRes = await axios.get(`https://headlight-tournament-3.herokuapp.com/bots`)
+        const nodesRes = await axios.get(`https://headlight-tournament-3.herokuapp.com/nodes`)
+        const botsData = botsRes.data.Bots
+        const nodesData = nodesRes.data.Nodes
+        const bots = processApiData(botsData)
+        const nodes = processApiData(nodesData)
+        this.setState({bots, nodes})
       }catch(err){
         console.log(err)
       }
     }
-    const intervalId = setInterval(makeCalls, 10000)
-    this.setState(intervalId)
+    const intervalId = setInterval(makeCalls, 1000)
+    this.setState({intervalId})
   }
 
   componentWillUnmount = () => {
@@ -37,9 +67,23 @@ class Map extends React.Component{
   // LIFECYCLE METHODS END1
 
   render(){
+    const rows = new Array(this.ROWS).fill(0)
+    const {bots, nodes} = this.state
+    // console.log(bots)
+    // console.log(nodes)
     return (
-      <div>
-        The map
+      <div id="map">
+        {
+          rows.map((_, i) => (
+            <Row
+              COLUMNS={this.COLUMNS}
+              row={i}
+              bots={bots}
+              nodes={nodes}
+              key={shortid.generate()}
+            />
+          ))
+        }
       </div>
     )
   }
